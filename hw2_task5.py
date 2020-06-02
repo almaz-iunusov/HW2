21bc6e4d901e2331eb00281ccd32565718fdfd85
Almaz Iunusov <65536757+almaz-iunusov@users.noreply.github.com>
2020-06-02 19:42:30 +0300

    hw2_task5: bugs fixed

--- hw2_task5.py
+++ hw2_task5.py
@@ -3,10 +3,13 @@ n = int(input("Введите новый элемент рейтинга"))
 if n == 1:
     myList.append(n)
     print("Дополненный рейтинг теперь выглядит так:", myList)
-for el in myList:
-    if n == el:
-        myList.insert((myList.index(n) + 1), n)
-        print("Дополненный рейтинг теперь выглядит так:", myList)
-    elif n > el:
-        myList.insert((myList.index(el)), n)
-        print("Дополненный рейтинг теперь выглядит так:", myList)
+else:
+    for el in myList:
+        if n == el:
+            myList.insert((myList.index(n) + 1), n)
+            print("Дополненный рейтинг теперь выглядит так:", myList)
+            break
+        elif n > el:
+            myList.insert((myList.index(el)), n)
+            print("Дополненный рейтинг теперь выглядит так:", myList)
+            break
